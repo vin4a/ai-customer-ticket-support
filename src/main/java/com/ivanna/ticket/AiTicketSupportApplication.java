18db1d4dc2bd20eea9e1793ddc3d54b1e216bb30
ivanna <ivanna.tovar@alumnos.upm.es>
2026-07-10 17:55:06 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ivanna/ticket/AiTicketSupportApplication.java
@@ -0,0 +1,13 @@
+package com.ivanna.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTicketSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTicketSupportApplication.class, args);
+	}
+
+}
