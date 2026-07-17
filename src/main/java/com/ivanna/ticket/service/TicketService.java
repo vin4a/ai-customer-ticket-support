b6662d1058aed4d14e9d312e465d7930bb36881b
vin4a <ivanna.tovar@proton.me>
2026-07-18 00:40:56 +0200

    chore: injection between service and repo Refs #6

--- src/main/java/com/ivanna/ticket/service/TicketService.java
+++ src/main/java/com/ivanna/ticket/service/TicketService.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TicketService{
 
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository){
+        this.ticketRepository = ticketRepository;
+    }
 }
