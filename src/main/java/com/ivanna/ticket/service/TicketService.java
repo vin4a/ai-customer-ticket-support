5a975012434f48059822697d48ed3001012ea0b0
vin4a <ivanna.tovar@proton.me>
2026-07-18 15:59:37 +0200

    feat: added header of CRUD methods Refs #12

--- src/main/java/com/ivanna/ticket/service/TicketService.java
+++ src/main/java/com/ivanna/ticket/service/TicketService.java
@@ -1,7 +1,11 @@
 package com.ivanna.ticket.service;
 
+import com.ivanna.ticket.model.Ticket;
+import com.ivanna.ticket.model.TicketStatus;
 import com.ivanna.ticket.repository.TicketRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketService{
@@ -11,4 +15,26 @@ public class TicketService{
     public TicketService(TicketRepository ticketRepository){
         this.ticketRepository = ticketRepository;
     }
+
+    public Ticket createTicket(Ticket ticket){
+        return null;
+    }
+
+    public List<Ticket> getAllTickets(){
+        return null;
+    }
+
+    public Ticket getTicketByID(Long id){
+
+        return null;
+    }
+
+    public Ticket updateTicketStatus(Long id, TicketStatus status){
+        return null;
+    }
+
+    public void deleteTicket(Long id){
+
+    }
+
 }
