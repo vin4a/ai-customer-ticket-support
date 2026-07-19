d4e063abe59678fe587f2e39d635873971a80e59
vin4a <ivanna.tovar@proton.me>
2026-07-19 15:17:25 +0200

    refactor: finished CRUD methods Refs #12

--- src/main/java/com/ivanna/ticket/service/TicketService.java
+++ src/main/java/com/ivanna/ticket/service/TicketService.java
@@ -1,10 +1,12 @@
 package com.ivanna.ticket.service;
 
 import com.ivanna.ticket.model.Ticket;
+import com.ivanna.ticket.model.TicketPriority;
 import com.ivanna.ticket.model.TicketStatus;
 import com.ivanna.ticket.repository.TicketRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,24 +19,28 @@ public class TicketService{
     }
 
     public Ticket createTicket(Ticket ticket){
-        return null;
+        ticket.setStatus(TicketStatus.OPEN);
+        ticket.setPriority(TicketPriority.MEDIUM);
+        ticket.setCreatedAt(LocalDateTime.now());
+        return ticketRepository.save(ticket);
     }
 
     public List<Ticket> getAllTickets(){
-        return null;
+        return ticketRepository.findAll();
     }
 
-    public Ticket getTicketByID(Long id){
-
-        return null;
+    public Ticket getTicketById(Long id){
+        return ticketRepository.findById(id).orElseThrow();
     }
 
     public Ticket updateTicketStatus(Long id, TicketStatus status){
-        return null;
+        Ticket ticket = getTicketById(id);
+        ticket.setStatus(status);
+        return ticketRepository.save(ticket);
     }
 
     public void deleteTicket(Long id){
-
+        ticketRepository.deleteById(id);
     }
 
 }
