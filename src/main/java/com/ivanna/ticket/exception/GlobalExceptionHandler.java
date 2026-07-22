5ada79be496282ca3c1e7fbc6f0fccea7b56ce50
vin4a <ivanna.tovar@proton.me>
2026-07-22 22:18:29 +0200

    feat: complete GlobalExceptionHandler class Refs #16

--- src/main/java/com/ivanna/ticket/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ivanna/ticket/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.ivanna.ticket.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TicketNotFoundException.class)
+    public ResponseEntity<String> handleTicketNotFound(TicketNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
