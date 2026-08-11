33001cc71b57fcfc2f0107a098c0e143876ab98f
vin4a <ivanna.tovar@proton.me>
2026-08-11 02:53:10 +0200

    feat: Actually Created userRequest class Refs #28

--- src/main/java/com/ivanna/ticket/config/SecurityConfig.java
+++ src/main/java/com/ivanna/ticket/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ivanna.ticket.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +18,11 @@ public class SecurityConfig {
                         .anyRequest().permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
 
