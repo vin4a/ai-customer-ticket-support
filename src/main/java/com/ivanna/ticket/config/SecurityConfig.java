51e7458706fa766481cd7fd9763ef8b5d00d2724
vin4a <ivanna.tovar@proton.me>
2026-08-13 00:22:58 +0200

    refactor: actual added AuthenticationManager Refs #29

--- src/main/java/com/ivanna/ticket/config/SecurityConfig.java
+++ src/main/java/com/ivanna/ticket/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ivanna.ticket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,5 +26,12 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
 }
 
